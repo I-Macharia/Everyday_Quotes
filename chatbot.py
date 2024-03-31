c381c0061abef0c56257a1033ba5b5d5e2c19b67
imacharia <macharia.gichoya@gmail.com>
2024-03-31 22:36:15 +0300

    updated chatbot with wake words. created deployment app using Taipy with error. updated requirements with taipy dependencies

--- chatbot.py
+++ chatbot.py
@@ -7,23 +7,33 @@ import transformers
 import random
 import pandas as pd
 
+df = pd.read_csv(r'E:\Documents\data_science\post_capstone\Everyday_Quotes\Everyday_Quotes\data\quotes_3.csv')
 
 def text_to_speech(text):
     print("AI --> ", text)
-    # speaker = gTTS(text=text, lang="en", slow=False)
+    speaker = gTTS(text=text, lang="en", slow=False)
     # speaker.save("res.mp3")
     # os.system("start res.mp3")  #if you have a macbook->afplay or for windows use->start
     # os.remove("res.mp3")
 
 
 class ChatBot:
-    def __init__(self, name):
+    def __init__(self, name, df):
         self.name = name
+        self.df = df
 
     def wake_up(self, text):
-        wake_words = ["dev", "bot", "assistant", "time", "hello", "quote"]
+        wake_words = ["dev", "bot", "assistant", "time", "hello", "quote", "thanks", "no", "yes"]
         return any(word in text.lower() for word in wake_words)
 
+    @staticmethod
+    def text_to_speech(text):
+        print("AI --> ", text)
+        speaker = gTTS(text=text, lang="en", slow=False)
+        # speaker.save("res.mp3")
+        # os.system("start res.mp3")  #if you have a macbook->afplay or for windows use->start
+        # os.remove("res.mp3")
+    
     def speech_to_text(self):
         r = sr.Recognizer()
         with sr.Microphone() as source:
@@ -41,28 +51,45 @@ class ChatBot:
         return datetime.datetime.now().time().strftime('%H:%M')
     
     @staticmethod
-    def get_random_quote(df):
-        return df.sample().quote.iloc[0]
+    def get_random_quote(df, topic=None):
+        if not topic:
+            return df.sample().quote.iloc[0]
+        filtered_quotes = df[df.category == topic]
+        if filtered_quotes.empty:
+            return f"Sorry, I couldn't find a quote on the topic '{topic}'."
+        return filtered_quotes.sample().quote.iloc[0]
 
 if __name__ == "__main__":
     df = pd.read_csv(r'E:\Documents\data_science\post_capstone\Everyday_Quotes\Everyday_Quotes\data\quotes_3.csv')
 
-    ai = ChatBot(name="Dev")
+    ai = ChatBot(name="Dev", df=df)
     while True:
         user_input = input("You --> ")
         ai.text = user_input
 
         if ai.wake_up(ai.text):
             if "time" in ai.text:
-                res = ai.action_time()
+                res = f"The time is {ai.action_time()}!"
             elif "quote" in ai.text:
-                res = ai.get_random_quote(df)
+                topic =""
+                words = ai.text.split()
+                for word in words:
+                    if "about" in word.lower() and "a" in word.lower() and "topic" in word.lower():
+                        index = words.index(word)
+                        topic = " ".join(words[index+1:]).strip()
+                res = ai.get_random_quote(ai.df, topic)
             elif "hello" in ai.text:
-                res = f"Hello, {ai.name}!"
+                res = f"Hello, my name is {ai.name}!"
+            elif "thanks" in ai.text:
+                res = "Welcome, is there anything else you would like me to help you with?"
+            elif "yes" in ai.text:
+                res =  "Great! How can I assist you today"
+            elif "no" in ai.text.lower() or "im fine" in ai.text.lower():
+                print("Bye! Have a great day!")
+                break
             else:
                 res = "I'm here to help you with time or quotes. What else can I do for you?"
         else:
             res = "Sorry, I didn't catch that."
 
         text_to_speech(res)
-        
